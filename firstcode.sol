460a3339846c76dcb1484d3cb06d82b7d8326f21
mirzaaghazadeh <mirzaaghazadeh@icloud.com>
2024-10-05 15:05:40 +0300

    first commit

---
+++ firstcode.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.26; // this is our version
+
+
+contract firstcontract{
+  // Bool , UINT , INT , BYTE , address , string
+
+    bool isAlive = true; 
+    uint age = 26;
+    uint height = 170;
+    string name = "navid";
+    address wallet = 0x13F2241aa64bb6DA2B74553fA9E12B713b74F334;
+    int score = -5; // int256
+
+
+    int score2 = -5;
+
+}
---
+++ storage.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.26; // this is our version
+
+contract simpleStorage{
+
+    uint public favoriteNumber; // 0
+
+    string public name; 
+
+    function store(uint number) public{
+        favoriteNumber = number;
+    }
+
+    function getNumber() public view returns(uint256){
+        return favoriteNumber + 1;
+    }
+
+     function getFixedNumber() public pure returns(uint256){
+        return  6;
+    }
+
+
+    function getXNumber(uint m) public view returns(uint256){
+        return favoriteNumber * m;
+    }
+
+
+    function saveNmae(string memory _name) public{
+        name = _name;
+    }
+
+
+
+} 
